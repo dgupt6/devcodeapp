00999cd75c7c6ed433b43161f1ce177a200f782a
Devraj Gupta <gupta.devraj@gmail.com>
2025-02-03 17:45:47 -0700

    added progress bar

--- codereviewerapp_withclass.py
+++ codereviewerapp_withclass.py
@@ -237,7 +237,7 @@ class StreamlitApp:
         my_bar = st.progress(0, text=progress_text)
 
         for percent_complete in range(100):
-            time.sleep(0.01)
+            time.sleep(0.10)
             my_bar.progress(percent_complete + 1, text=progress_text)
         time.sleep(1)
         my_bar.empty()
